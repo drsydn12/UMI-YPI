ee3f6708460d1a5a9386418ad46927c63f4a0e78
DIFA <42770277+green404@users.noreply.github.com>
2022-05-23 18:22:55 +0700

    Add files via upload

---
+++ UMIWAB.js
@@ -0,0 +1,10 @@
+{
+	"version": "15.0",
+	"download": "https://difarosyidin.xyz",
+	"update": [
+		{
+			"languaje": "in",
+			"changelog": "https://raw.githubusercontent.com/green404/UMI/main/changelog-B-in.html"
+		}
+	]
+}
